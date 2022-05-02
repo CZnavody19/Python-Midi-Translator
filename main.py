26095f8e587a1bcd7b3ae53484ada3a70e48306c
CZnavody19 <cznavody19@gmail.com>
2022-05-02 17:57:15 +0200

    added yaml support

--- main.py
+++ main.py
@@ -1,48 +1,140 @@
-import rtmidi
+import rtmidi as midi
+import keyboard
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume, IAudioEndpointVolume
+import psutil
+from numpy import interp
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+import yaml
+
+port = None
+
+volumes = {}
+
+pot = {}
+pad = {}
+
+def slider(id, value):
+    norm_val = interp(value, [0,127], [0,1])
+    message = "Set volume of {} to {}%"
+
+    try:
+        if pot[id] == "main":
+            volumes[id].SetMasterVolumeLevelScalar(norm_val, None)
+            print(message.format(pot[id], round(norm_val*100)))
+        else:
+            volumes[id].SetMasterVolume(norm_val, None)
+            print(message.format(pot[id], round(norm_val*100)))
+    
+    except KeyError:
+        pass
+
+def pad_on(id):
+    try:
+        key = pad[id]
+        keyboard.press_and_release(key)
+        print("Pressed {}".format(key))
+
+    except KeyError:
+        pass
+
+def pad_off(id):
+    pass
+
+def prg_swtch(id):
+    pass
+
+
+def findProcessIdByName(processName):
+    '''
+    Get a list of all the PIDs of a all the running process whose name contains
+    the given string processName
+    '''
+    listOfProcessObjects = []
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+       try:
+           pinfo = proc.as_dict(attrs=['pid', 'name'])
+           # Check if process name contains the given name string.
+           if processName.lower() in pinfo['name'].lower() :
+               listOfProcessObjects.append(pinfo['pid'])
+       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+           pass
+    return listOfProcessObjects
+
+def get_app_volume_obj(name):
+    sessions = AudioUtilities.GetAllSessions()
+    processid = findProcessIdByName(name)
+    for session in sessions:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        for proc_id in processid:
+            if session.ProcessId == proc_id:
+                print("Found session with id {}".format(session.ProcessId))
+                return volume
+
+def get_master_volume_obj():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    print("Got main volume!")
+    return volume
+
+def init_volume():
+    global volumes
+
+    for vol in pot:
+        vol_name = pot[vol]
+        if vol_name == "main":
+            volumes[vol] = get_master_volume_obj()
+        else:
+            volumes[vol] = get_app_volume_obj(vol_name)
+
+def get_config():
+    global port
+    global pad
+    global pot
+
+    config = None
+
+    with open("config.yml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError:
+            print("Could not read config file!")
+
+    port = config["setup"]["port"]
+    pad = config["PAD"]
+    pot = config["POT"]
+
 
 def main():
-    midiin = rtmidi.MidiIn(name="Midi Translator")
+    get_config()
 
-    get_ports_out = get_ports(midiin)
-    if get_ports_out == False:
-        print("No ports found!")
+    init_volume()
+
+    midi_in = midi.MidiIn()
+    try:
+        opened_port = midi_in.open_port(port)
+        print("Opened port {}".format(port))
+    except (midi._rtmidi.SystemError, midi._rtmidi.InvalidPortError, midi._rtmidi.RtMidiError):
+        print("Invalid port selected ({})".format(port))
         raise KeyboardInterrupt
-    else:
-        print(get_ports_out)
 
     while True:
-        try:
-            port_num = int(input("Port to open: "))
-            midiin.open_port(port_num)
-            print("Opened port {}.".format(port_num))
-            break
-        except (ValueError, rtmidi._rtmidi.InvalidPortError, rtmidi._rtmidi.RtMidiError):
-            print("Enter valid port number!")
-
-    while True:
-        message = midiin.get_message()
+        message = opened_port.get_message()
         if message != None:
-            message = message[0]
-
-            #sort messages             add yaml parser
-
-
-
-
-def get_ports(midiin):
-    ports = midiin.get_ports()
-    msg = ""
-    if len(ports) == 0:
-        return False
-    else:
-        for port in ports:
-            splitted = port.split(" ")
-            msg += "Port {} with name {} found!\n".format(splitted[1], splitted[0])
-        return msg
-
-
+            mode = message[0][0]
+            if mode	== 176:
+                slider(message[0][1], message[0][2])
+            elif mode == 144:
+                pad_on(message[0][1])
+            elif mode == 128:
+                pad_off(message[0][1])
+            elif mode == 192:
+                prg_swtch(message[0][1])
+ 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("Shutting down program!")
+        print("Shutting down app!")
